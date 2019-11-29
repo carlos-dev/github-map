154e168fd7a81fccdbf1737811e794725dae50f3
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2019-11-29 15:31:31 -0300

    criando mapa

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,9 +1,54 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import ReactMapGL from 'react-map-gl';
 
-export default class componentName extends Component {
+import "mapbox-gl/dist/mapbox-gl.css";
+
+export default class Map extends Component {
+
+  state = {
+    viewport: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      latitude: -23.0038303,
+      longitude: -43.3233906,
+      zoom: 9,
+    }
+  };
+
+  handleMapClick = (e) => {
+    const [latitude, longitude] = e.lngLat;
+
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this._resize);
+    this._resize();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this._resize);
+  }
+
+  _resize = () => {
+    this.setState({
+      viewport: {
+        ...this.state.viewport
+      }
+    });
+
+    console.log(this.state)
+  };
+
   render() {
     return (
-      <div> textInComponent </div>
+      <ReactMapGL
+        onClick={this.handleMapClick}
+        mapStyle="mapbox://styles/mapbox/basic-v9"
+        mapboxApiAccessToken={'pk.eyJ1IjoiYmV0YWZsaXAiLCJhIjoiY2sza2QxbmpiMHNubTNjbXZwNml0aWQ0NyJ9.LmPTU7uoUOQ7110LTlw8sw'}
+        {...this.state.viewport}
+        onViewportChange={(viewport) => this.setState({viewport})}
+      />
     );
   }
 }
+
